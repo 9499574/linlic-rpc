baa8a51374807f541ff856b253f070ea445435df
liujiaxiang <9499574@qq.com>
2024-01-17 14:37:52 +0800

    OSS文件上传修改

--- src/JsonRpc/OssTempFileServiceInterface.php
+++ src/JsonRpc/OssTempFileServiceInterface.php
@@ -9,8 +9,9 @@ interface OssTempFileServiceInterface
      * @param int $orgId 机构编号
      * @param int $uId 用户编号
      * @param string $module 模块名称
+     * @param string $fileName 原文件名称
      * @param string $ossUrl 临时文件地址
      * @return array
      */
-    public function fileTransferBucket(int $orgId, int $uId, string $module, string $ossUrl): array;
+    public function fileTransferBucket(int $orgId, int $uId, string $module,string $fileName, string $ossUrl): array;
 }
