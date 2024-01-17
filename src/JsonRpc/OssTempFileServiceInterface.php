9a7ccb475ff214e7eecfe2e72306067e6fe05977
liujiaxiang <9499574@qq.com>
2024-01-17 13:49:57 +0800

    OSS临时文件转移

---
+++ src/JsonRpc/OssTempFileServiceInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace linlic\JsonRpc;
+
+interface OssTempFileServiceInterface
+{
+    /**
+     * 临时文件转机构目录
+     * @param int $orgId 机构编号
+     * @param int $uId 用户编号
+     * @param string $module 模块名称
+     * @param string $ossUrl 临时文件地址
+     * @return array
+     */
+    public function fileTransferBucket(int $orgId, int $uId, string $module, string $ossUrl): array;
+}
