3c7a2840f3515c7d63cf959e9376c71e4a1ed2ac
pengxiaolong <756243171@qq.com>
2024-01-22 18:14:18 +0800

    调整用户中心接口

--- src/JsonRpc/OperateServiceInterface.php
+++ src/JsonRpc/OperateServiceInterface.php
@@ -5,8 +5,9 @@ namespace linlic\JsonRpc;
 interface OperateServiceInterface
 {
     /**
-     * 获取用户列表
+     * 根据用户编号返回用户姓名
+     * @param array $uid
      * @return array
      */
-    public function getUserList(): array;
+    public function getUserName(array $uid): array;
 }
