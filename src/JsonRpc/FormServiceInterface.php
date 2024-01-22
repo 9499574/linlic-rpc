05f678a87bcf9dd87c064f6c9b3af362b29980e2
zhichuangxin <924535840@qq.com>
2024-01-22 10:39:14 +0800

    获取填报表单字段接口

--- src/JsonRpc/FormServiceInterface.php
+++ src/JsonRpc/FormServiceInterface.php
@@ -11,27 +11,11 @@ interface FormServiceInterface
     public function getFillingForms():array;
 
     /**
-     * 设置填报表单配置
-     * @param string $org_id 机构id
-     * @param string $uid 登录用户uid
-     * @param string $system_id 系统id
-     * @param string $menu_id 菜单id
-     * @param string $filling_form_template_id 填报表单id
-     * @return void
-     */
-    public function setFillingFormConfig(
-        string $org_id,
-        string $uid,
-        string $system_id,
-        string $menu_id,
-        string $filling_form_template_id
-    ):void;
-
-    /**
      * 获取填报表单字段
+     * @param string $filling_form_template_id 填报表单ID
      * @return array
      */
-    public function getFillingFormFields():array;
+    public function getFillingFormFields(string $filling_form_template_id):array;
 
     /**
      * 获取可用的个人资料模板
