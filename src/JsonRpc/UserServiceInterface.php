01d4631e1db3a41793fe894721541fbb64e3a74e
zhuhai <ftp504@163.com>
2024-01-23 20:43:56 +0800

    用户中心rpc的最新方法

--- src/JsonRpc/UserServiceInterface.php
+++ src/JsonRpc/UserServiceInterface.php
@@ -4,12 +4,20 @@ namespace linlic\JsonRpc;
 
 interface UserServiceInterface
 {
+    public function test($a,$b):int;
     /*
-     * 机构添加管理员账号
-     * @param string $orgId 机构编号
+     * 机构设置管理员账号
+     * @param string $orgId 机构的id编号
      * @param string $userName 管理员账号的用户名
-     * @return bool
+     * @return array。如：['status'=>$status,'message'=>$message]
      */
     public function synManageUser($orgId,$userName):array;
-    public function test($a,$b):int;
+
+    /*
+    * 根据机构id，获取到机构下用户的数量信息
+    * @param string $orgId 机构的id编号
+    * @return int。如：20
+    */
+    public function getOrgUserCount($orgId):array;
+
 }
