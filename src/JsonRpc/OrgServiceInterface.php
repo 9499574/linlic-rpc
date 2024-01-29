24b96c0f272d73df803d6123d56b58a473b355bf
赵东升 <690617211@qq.com>
2024-01-29 13:32:20 +0800

    机构rpc add 获取机构信息

---
+++ src/JsonRpc/OrgServiceInterface.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace linlic\JsonRpc;
+
+interface OrgServiceInterface
+{
+    /**
+     * 获取机构信息
+     *
+     * @param int $orgId 机构ID
+     * @return array
+     * // 返回示例
+     * //   [
+     * //     "id": 48,
+     * //     "org_id": 610427129594953728,
+     * //     "org_name": "成都市第七人民医院",
+     * //     "customer_id": 19009,
+     * //     "org_logo": "610427135601168384",
+     * //     "org_status": 1,
+     * //     "account_type": 1,
+     * //     "org_account": "cddqrmyy",
+     * //     "org_domain": "cddqrmyy.ccmtv.cn",
+     * //     "account_num": 0,
+     * //     "storage_capacity": 0,
+     * //     "sms_num": 0,
+     * //     "created_at": "2024-01-29 10:59:28",
+     * //     "updated_at": "2024-01-29 10:59:30",
+     * //     "deleted_at": null,
+     * //     "created_by": 608729674080534529,
+     * //     "updated_by": 608729674080534529,
+     * //     "deleted_by": 0
+     * //   ]
+     * @user zhaodongsheng
+     * @time 2024/1/29 10:57
+     */
+    public function getInfoByOrgId(string $orgId): array;
+}
