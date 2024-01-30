aabf800623ceaa6752b0561a4a62d109cf08630e
liujiaxiang <9499574@qq.com>
2024-01-30 20:06:33 +0800

    通过菜单ID获取详情数据

---
+++ src/JsonRpc/SystemMenuServiceInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace linlic\JsonRpc;
+
+interface SystemMenuServiceInterface
+{
+    /**
+     * 通过菜单ID获取详情数据
+     * @param array $menu_id
+     * @return array
+     */
+    public function menuIdGetDataList(array $menu_id):array;
+}
