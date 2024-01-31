c9ecde41bc8056d0505259d3fde84f15d3fb64ee
liujiaxiang <9499574@qq.com>
2024-01-31 09:32:59 +0800

    通过菜单ID获取详情数据

--- src/JsonRpc/SystemMenuServiceInterface.php
+++ src/JsonRpc/SystemMenuServiceInterface.php
@@ -10,4 +10,11 @@ interface SystemMenuServiceInterface
      * @return array
      */
     public function menuIdGetDataList(array $menu_id):array;
+
+    /**
+     * 通过系统ID获取菜单
+     * @param $system_id
+     * @return array
+     */
+    public function getSystemMenuList($system_id):array;
 }
