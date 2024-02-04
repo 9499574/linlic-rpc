e6a800e6f4019fa29713495bc92eb95f7a8f3229
liujiaxiang <9499574@qq.com>
2024-02-04 13:35:24 +0800

    系统模块功能菜单PRC方法

--- src/JsonRpc/SystemMenuServiceInterface.php
+++ src/JsonRpc/SystemMenuServiceInterface.php
@@ -5,16 +5,23 @@ namespace linlic\JsonRpc;
 interface SystemMenuServiceInterface
 {
     /**
-     * 通过菜单ID获取详情数据
-     * @param array $menu_id
-     * @return array
-     */
-    public function menuIdGetDataList(array $menu_id):array;
-
-    /**
-     * 通过系统ID获取菜单
+     * 通过系统编号获取系统关联的功能配置
      * @param $system_id
      * @return array
      */
-    public function getSystemMenuList($system_id):array;
+    public function getSystemModuleFunConf($system_id):array;
+
+    /**
+     * 通过模块功能编号获取表数据
+     * @param array $fun_id
+     * @return array
+     */
+    public function moduleFunIdGetData(array $fun_id):array;
+
+    /**
+     * 通过菜单编号获取表数据
+     * @param array $menu_id
+     * @return array
+     */
+    public function menuIdGetData(array $menu_id):array;
 }
