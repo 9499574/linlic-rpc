355fec8ddea3fd28362cabfdbbbd11457269b64c
pengxiaolong <756243171@qq.com>
2024-03-11 16:10:09 +0800

    本院科室rpc新增接口

--- src/JsonRpc/HospitalDepartmentServiceInterface.php
+++ src/JsonRpc/HospitalDepartmentServiceInterface.php
@@ -10,4 +10,14 @@ interface HospitalDepartmentServiceInterface
      */
     public function getConfigs():array;
 
+
+    /**
+     * 根据机构id，获取本院科室选项
+     * @param string $orgId
+     * @param array $where ['option_label'=>["眼科"],'option_value'=>["622116875481731073"]]
+     * @param array $select ['option_label','option_value']
+     * @return array [["option_label"=>"眼科","option_value"=>"622116875481731073"]]
+     */
+    public function getOptions(string $orgId,array $where=[],array $select=[]):array;
+
 }
