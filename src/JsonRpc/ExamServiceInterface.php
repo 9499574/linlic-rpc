938c487c12b046e11d8c15ab4efc79e4a1dd695c
shenyilong <shenyilong@linlic.onaliyun.com>
2024-03-12 20:02:35 +0800

    考试RPC接口

---
+++ src/JsonRpc/ExamServiceInterface.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace linlic\JsonRpc;
+
+interface ExamServiceInterface
+{
+
+    /**
+     * 获取考试字段
+     * @param array $params 参数数组
+     * @return array
+     */
+    public function getExamFields(array $params = []):array;
+
+    /**
+     * 根据机构id，获取考试选项
+     * @param string $orgId
+     * @param array $select ['option_label','option_value']
+     * @param array $where ['option_label'=>["眼科"],'option_value'=>["622116875481731073"]]
+     * @return array [["option_label"=>"眼科","option_value"=>"622116875481731073"]]
+     */
+    public function getExamOptions(string $orgId,array $select=[],array $where=[]):array;
+
+}
