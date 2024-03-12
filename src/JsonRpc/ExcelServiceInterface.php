106bee73296b7c3664b143fb9287d2a574c25752
liujiaxiang <9499574@qq.com>
2024-03-12 13:35:36 +0800

    增加excel RPC方法

---
+++ src/JsonRpc/ExcelServiceInterface.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace linlic\JsonRpc;
+
+interface ExcelServiceInterface
+{
+    /**
+     * 下载excel文件
+     * @param array $userInfo ['uid'=>1111,'org_id'=>1111]
+     * @param string $fileName aaaa.xlxs
+     * @param array $header [["表头1",""表头2"]]
+     * @param array $data [["数据1","数据2"]]
+     * @param array $options ['mergeCells'=>['A1:B1'],'sheetName'=>'Sheet1','NumberFormat'=>[['column'=>'B','formate'=>'yyyy-mm-dd']]]
+     * @return array
+     */
+    public function export(array $userInfo,string $fileName,array $header,array $data,array $options=[]):array;
+
+    /**
+     * 下载excel文件
+     * 多sheet
+     * @param array $userInfo
+     * @param string $fileName
+     * @param array $params [['header'=>[],'data'=>[],'options'=>[]],['header'=>[],'data'=>[],'options'=>[]]]
+     * @return array
+     */
+    public function exportManySheet(array $userInfo,string $fileName,array $params): array;
+
+
+    /**
+     * 获取excel文件内容
+     * @param string $fileName
+     * @param string $ossUrl
+     * @param bool $deleteOssFile
+     * @return array
+     */
+    public function getContent(string $fileName,string $ossUrl, bool $deleteOssFile=true): array;
+}
