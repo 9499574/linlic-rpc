4d36ea48c3dc5b18b22198bcc22bf8f123af8d7d
shenyilong <shenyilong@linlic.onaliyun.com>
2024-03-13 11:39:39 +0800

    考试RPC接口

--- src/JsonRpc/ExamServiceInterface.php
+++ src/JsonRpc/ExamServiceInterface.php
@@ -6,11 +6,11 @@ interface ExamServiceInterface
 {
 
     /**
-     * 获取考试字段
+     * 获取考试发布表单字段
      * @param array $params 参数数组
      * @return array
      */
-    public function getExamFields(array $params = []):array;
+    public function getExamFormFields(array $params = []):array;
 
     /**
      * 根据机构id，获取考试选项
