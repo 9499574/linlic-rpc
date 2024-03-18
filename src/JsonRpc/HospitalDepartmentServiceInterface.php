9369ef237f8b9b2da06df9ff51f0c893d5ad1570
zhichuangxin <924535840@qq.com>
2024-03-18 18:25:58 +0800

    获取本院科室列表数据

--- src/JsonRpc/HospitalDepartmentServiceInterface.php
+++ src/JsonRpc/HospitalDepartmentServiceInterface.php
@@ -20,4 +20,14 @@ interface HospitalDepartmentServiceInterface
      */
     public function getOptions(string $orgId,array $where=[],array $select=[]):array;
 
+    /**
+     * 获取本院科室列表数据
+     * @param string $orgId 机构id
+     * @param array $select 查询字段
+     * @param array $where 查询条件
+     * @param array $pageParams 分页参数['page'=>1,'limit'=>15] 空数组表示不分页
+     * @return array
+     */
+    public function getHospitalDepartmentList(string $orgId,array $select,array $where,array $pageParams=[]):array;
+
 }
