c77d5b7af5c0d630ab81e84830cb0f0ba9c058a7
liujiaxiang <9499574@qq.com>
2024-03-19 11:56:21 +0800

    排班记录-模块字段增加PRC接口

--- src/JsonRpc/CyclePlanServiceInterface.php
+++ src/JsonRpc/CyclePlanServiceInterface.php
@@ -4,7 +4,24 @@ namespace linlic\JsonRpc;
 
 interface CyclePlanServiceInterface
 {
+    /**
+     * 新建排班-模块字段
+     * @param $params ['org_id'=>-1]
+     * @return array
+     */
     public function planField($params):array;
 
+    /**
+     * 新建排班-配置字段
+     * @param $params ['org_id'=>-1]
+     * @return array
+     */
     public function planConf($params):array;
+
+    /**
+     * 排班记录-模块字段
+     * @param $params ['org_id'=>-1]
+     * @return array
+     */
+    public function planLogField($params):array;
 }
