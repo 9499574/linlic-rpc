ed300d76590285d963bc45e6fcfd52fae44190ab
liujiaxiang <9499574@qq.com>
2024-03-22 11:06:42 +0800

    科室关系RPC接口

--- src/JsonRpc/SdHdRelationServiceInterface.php
+++ src/JsonRpc/SdHdRelationServiceInterface.php
@@ -7,14 +7,16 @@ interface SdHdRelationServiceInterface
     /**
      * 通过标准科室ID获取对应本院科室ID
      * @param array $sdIds
+     * @param int $org_id
      * @return array ['111'=>[111,2222],'2222'=>[3333,444]]
      */
-    public function getSdIdByHdId(array $sdIds):array;
+    public function getSdIdByHdId(array $sdIds,int $org_id):array;
 
     /**
      * 通过本院科室ID获取对应标准科室ID
      * @param array $hdIds
+     * @param int $org_id
      * @return array ['111'=>[111,2222],'2222'=>[3333,444]]
      */
-    public function getHdIdBySdId(array $hdIds):array;
+    public function getHdIdBySdId(array $hdIds,int $org_id):array;
 }
