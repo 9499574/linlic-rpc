14be4efdab5f8ef44d01c37041604a6aacc84457
liujiaxiang <9499574@qq.com>
2024-03-28 17:30:55 +0800

    本院科室与标准科室关联选项 返回级联数据格式

--- src/JsonRpc/SdHdRelationServiceInterface.php
+++ src/JsonRpc/SdHdRelationServiceInterface.php
@@ -19,4 +19,11 @@ interface SdHdRelationServiceInterface
      * @return array ['111'=>[111,2222],'2222'=>[3333,444]]
      */
     public function getHdIdBySdId(array $hdIds,int $org_id):array;
+
+    /**
+     * 本院科室与标准科室关联选项 返回级联数据格式
+     * @param int $org_id
+     * @return array
+     */
+    public function hdSdRelationCascaderOptions(int $org_id):array;
 }
