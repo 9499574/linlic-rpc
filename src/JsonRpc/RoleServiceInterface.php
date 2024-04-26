9dd644204e4b66a41caa37dd02ce99dd2e7d2b39
liujiaxiang <9499574@qq.com>
2024-04-26 20:04:00 +0800

    根据角色组id获取角色id

--- src/JsonRpc/RoleServiceInterface.php
+++ src/JsonRpc/RoleServiceInterface.php
@@ -23,4 +23,11 @@ interface RoleServiceInterface
      * @return array
      */
     public function getRoleList(int $org_id,string $system_id,string $id_key='role_id',string $name_key='role_name'):array;
+
+    /**
+     * 根据角色组id获取角色id
+     * @param array $role_id
+     * @return array
+     */
+    public function topPidGetChildRoleId(array $role_id):array;
 }
