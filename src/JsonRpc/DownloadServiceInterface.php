83b8ed6531778cae655ad0f8de7647cbcb8fa389
rango <living9696@hotmail.com>
2024-04-29 20:28:35 +0800

    下载rpc更新

--- src/JsonRpc/DownloadServiceInterface.php
+++ src/JsonRpc/DownloadServiceInterface.php
@@ -10,36 +10,23 @@ interface DownloadServiceInterface
      * @param string $orgId 机构id
      * @param string $moduleName 模块名称，如 exam-center
      * @param string $fileName 需要生成的文件名称
-     * @return array
+     * @return string 返回下载请求id request_record_id
      */
-    public function handleRequest(string $userId, string $orgId, string $moduleName, string $fileName):array;
+    public function handleRequest(string $userId, string $orgId, string $moduleName, string $fileName):string;
 
     /**
      * 生成导出文件
+     * $params参数结构如下
+     * [
+     *      ["header" => [["表头1",""表头2"]], "data" => [["数据1","数据2"]], "options" => ['mergeCells'=>['A1:B1'],'sheetName'=>'Sheet1','NumberFormat'=>[['column'=>'B','formate'=>'yyyy-mm-dd']]]],
+     *      ["header" => [["表头3",""表头4"]], "data" => [["数据3","数据4"]], "options" => ['mergeCells'=>['A1:B1'],'sheetName'=>'Sheet2','NumberFormat'=>[['column'=>'B','formate'=>'yyyy-mm-dd']]]],
+     * ]
+     * $param中的元素个数代表sheet个数, 当只有一个元素时，按单个sheet表格文件处理， options可为空
      * @param string $requestRecordId 下载请求id
-     * @param array $header [["表头1",""表头2"]]
-     * @param array $data [["数据1","数据2"]]
-     * @param array $options ['mergeCells'=>['A1:B1'],'sheetName'=>'Sheet1','NumberFormat'=>[['column'=>'B','formate'=>'yyyy-mm-dd']]]
-     * @return array
+     * @param array $params
+     * @return bool 是否生成成功
      */
-    public function generateFile(string $requestRecordId, array $header, array $data, array $options=[]):array;
+    public function generateFile(string $requestRecordId, array $params):bool;
 
-    /**
-     * 获取文件下载地址
-     * @param string $userId 用户id
-     * @param string $orgId 机构id
-     * @param string $moduleName 模块名称
-     * @param int $type 下载文件类型，1：导出文件下载 2：资源文件下载
-     * @param string $relatedId 关联文件id，type=1，给下载请求id，requestRecordId type=2, 给resourceId
-     * @return array
-     */
-    public function getDownloadUrl(string $userId, string $orgId, string $moduleName, int $type, string $relatedId): array;
-
-    /**
-     * 根据uid获取用户下载请求记录
-     * @param string $userId
-     * @return array
-     */
-    public function getRecordsByUser(string $userId): array;
 
 }
