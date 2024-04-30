8a312a4c2c77f7c495fc52e2e9acb48dca8bad5c
liujiaxiang <9499574@qq.com>
2024-04-30 16:17:13 +0800

    根据本院科室ID集合获取当前用户ID集合

--- src/JsonRpc/CyclePlanUserDetailServiceInterface.php
+++ src/JsonRpc/CyclePlanUserDetailServiceInterface.php
@@ -8,10 +8,11 @@ interface CyclePlanUserDetailServiceInterface
      * 根据本院科室ID集合获取当前用户ID集合
      * @param int $org_id
      * @param array $hd_ids
+     * @param array $uid
      * @param array $system_id
      * @param array $menu_id
      * @param string $operation
      * @return array
      */
-    public function hdIdGetNowUserId(int $org_id,array $hd_ids,array $system_id=[],array $menu_id=[],string $operation='in'):array;
+    public function hdIdGetNowUserId(int $org_id,array $hd_ids,array $uid=[],array $system_id=[],array $menu_id=[],string $operation='in'):array;
 }
