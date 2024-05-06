247f66bdd30a7ddd7a049081c41681fdc8a27e57
liujiaxiang <9499574@qq.com>
2024-05-06 18:10:49 +0800

    获取机构系统角色组初始化数据(系统初始化调用一次)

--- src/JsonRpc/RoleServiceInterface.php
+++ src/JsonRpc/RoleServiceInterface.php
@@ -15,6 +15,14 @@ interface RoleServiceInterface
     public function getRoleGroupList(int $org_id,string $system_id,string $id_key='role_id',string $name_key='role_name'):array;
 
     /**
+     * 获取机构系统角色组初始化数据(系统初始化调用一次)
+     * @param int $org_id
+     * @param string $system_id
+     * @return array
+     */
+    public function getRoleGroupInitList(int $org_id,string $system_id):array;
+
+    /**
      * 获取机构系统角色
      * @param int $org_id
      * @param string $system_id
