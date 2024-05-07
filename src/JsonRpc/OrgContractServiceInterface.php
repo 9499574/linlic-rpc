2c8a5e6072a77acd1e84785aad3918b493565a87
liujiaxiang <9499574@qq.com>
2024-05-07 13:06:27 +0800

    获取机构系统最后一个结束时间和状态

--- src/JsonRpc/OrgContractServiceInterface.php
+++ src/JsonRpc/OrgContractServiceInterface.php
@@ -11,4 +11,12 @@ interface OrgContractServiceInterface
      * @return array
      */
     public function getOrgSystemContractInfo(int $org_id, string $system_id):array;
+
+    /**
+     * 获取机构系统最后一个结束时间和状态
+     * @param int $org_id
+     * @param string $system_id
+     * @return array
+     */
+    public function getOrgSystemEndDateStatus(int $org_id, string $system_id):array;
 }
