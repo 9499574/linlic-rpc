db9c84bcfbfca283ee8aba48ca0de16cf836b663
liujiaxiang <9499574@qq.com>
2024-05-08 13:00:08 +0800

    考试自动发布根据参数获取用户ID集合

--- src/JsonRpc/CyclePlanUserDetailServiceInterface.php
+++ src/JsonRpc/CyclePlanUserDetailServiceInterface.php
@@ -27,4 +27,11 @@ interface CyclePlanUserDetailServiceInterface
      * @return array
      */
     public function hdIdGetDateRangeUserId(int $org_id,array $hd_ids,string $start_date,string $end_date,array $uid=[],string $operation='in'):array;
+
+    /**
+     * 考试自动发布根据参数获取用户ID集合
+     * @param array $params ['type'=>'1末次轮转|2每次轮转','hd_id'=>['本院科室id'],'sd_id'=>['标准科室id'],'stage'=>['1|2|3|99'],'standard_base_id'=>['基地1','基地2']]
+     * @return array
+     */
+    public function examAutoPublishGetUserId(array $params):array;
 }
