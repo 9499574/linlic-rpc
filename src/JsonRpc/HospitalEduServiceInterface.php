b7e2902fbf4d424ab908bc9ad917fdc67eb9d924
zhichuangxin <924535840@qq.com>
2024-05-23 17:51:51 +0800

    教育功能字段

---
+++ src/JsonRpc/HospitalEduServiceInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace linlic\JsonRpc;
+
+interface HospitalEduServiceInterface
+{
+
+    /**
+     * 功能字段
+     * @param array $params
+     * @return array
+     */
+    public function Field(array $params):array;
+
+}
