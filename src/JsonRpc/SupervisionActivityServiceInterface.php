d4b8b6d740be540a2225db2ae56c9b02b890e2a4
zhaichunke <1347841607@qq.com>
2024-05-29 10:19:49 +0800

    活动督导-功能字段

---
+++ src/JsonRpc/SupervisionActivityServiceInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace linlic\JsonRpc;
+
+interface SupervisionActivityServiceInterface
+{
+    /**
+     * 功能字段
+     * @param array $params
+     * @return array
+     */
+    public function supervisionField(array $params):array;
+
+    
+
+}
