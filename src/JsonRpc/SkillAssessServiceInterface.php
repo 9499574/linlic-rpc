7001dcfae26bc56861ecb9d74188c70cebebc6de
shenyilong <shenyilong@linlic.onaliyun.com>
2024-05-30 11:40:19 +0800

    添加技能考核配置接口

---
+++ src/JsonRpc/SkillAssessServiceInterface.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace linlic\JsonRpc;
+
+interface SkillAssessServiceInterface
+{
+
+    /**
+     * 技能考核-模块字段
+     * @param $params ['org_id'=>-1]
+     * @return array
+     */
+    public function skillAssessField($params):array;
+
+    /**
+     * 技能考核-配置字段
+     * @param $params ['org_id'=>-1]
+     * @return array
+     */
+    public function skillAssessConf($params):array;
+
+}
