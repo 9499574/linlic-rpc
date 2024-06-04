ab437b906b8f325caa1ef6273a5b3dc3cc370184
dbag <9499574@qq.com>
2024-06-04 20:37:17 +0800

    增加表单审核流程接口

---
+++ src/JsonRpc/FormFlowServiceInterface.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace linlic\JsonRpc;
+
+interface FormFlowServiceInterface
+{
+    /**
+     * 表单字段
+     * @param array $params
+     * @return array
+     */
+    public function params(array $params):array;
+
+    /**
+     * 流程回调
+     * @param array $params
+     * @return bool
+     */
+    public function callBack(array $params):bool;
+}
