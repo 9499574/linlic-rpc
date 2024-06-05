fc56bc0adf381538415274fe44e300b94edb867a
dbag <9499574@qq.com>
2024-06-05 13:59:20 +0800

    流程设置 PRC接口

---
+++ src/JsonRpc/FlowConfServiceInterface.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace linlic\JsonRpc;
+
+/**
+ * 流程设置 PRC接口
+ */
+interface FlowConfServiceInterface
+{
+    /**
+     * 功能字段
+     * @param array $params
+     * @return array
+     */
+    public function fields(array $params):array;
+
+    /**
+     * 菜单配置
+     * @param array $params
+     * @return array
+     */
+    public function config(array $params):array;
+}
