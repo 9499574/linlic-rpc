6fe4e5403d506d1eccf485b80e38e536597e5235
zhuhai <ftp504@163.com>
2024-06-13 14:24:01 +0800

    现场签到的字段配置信息

--- src/JsonRpc/OnSiteSignServiceInterface.php
+++ src/JsonRpc/OnSiteSignServiceInterface.php
@@ -2,17 +2,17 @@
 
 namespace linlic\JsonRpc;
 
-interface AskLeaveServiceInterface
+interface OnSiteSignServiceInterface
 {
     /**
-     * 列表-功能字段列表的接口
+     * 日常考勤列表-功能字段列表的接口
      * @param array $params
      * @return array
      */
     public function getFields(array $params):array;
 
     /**
-     * 列表-菜单设置的接口
+     * 日常考勤列表-菜单设置的接口
      * @param array $params
      * @return array
      */
