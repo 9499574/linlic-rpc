e40597c8e49e0317c3f75f51c79d53247a24936b
zhuhai <ftp504@163.com>
2024-06-17 21:43:55 +0800

    新增 rpc接口方法调用

--- src/JsonRpc/AttendanceServiceInterface.php
+++ src/JsonRpc/AttendanceServiceInterface.php
@@ -4,17 +4,26 @@ namespace linlic\JsonRpc;
 
 interface AttendanceServiceInterface
 {
-    /**
+    /*
+    * 考勤管理-根据方法名和参数，调用rpc接口
+    * @param string $funcName
+    * @param array $params
+    * @return array
+    */
+    public function getFuncName($funcName,array $params):array;
+
+    /*
      * 考勤管理-考勤列表-功能字段列表的接口
      * @param array $params
      * @return array
      */
     public function getFields(array $params):array;
 
-    /**
+    /*
      * 考勤管理-考勤列表-菜单设置的接口
      * @param array $params
      * @return array
      */
     public function getConfigs(array $params):array;
+
 }
