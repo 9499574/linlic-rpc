1fff33e0065927e52dee141280760dee3b2b5a26
zhichuangxin <924535840@qq.com>
2024-06-19 14:34:40 +0800

    获取出科人员

--- src/JsonRpc/CycleViewServiceInterface.php
+++ src/JsonRpc/CycleViewServiceInterface.php
@@ -46,15 +46,16 @@ interface CycleViewServiceInterface
 
     /**
      * 获取出科人员
-     * @param string $org_id 机构id
-     * @param array $menu_id 菜单数组
-     * @param array $hd_id 本院科室数组
-     * @param array $sd_id 标准科室数组
-     * @param array $stage 轮转阶段数组
-     * @param array $standard_base_id 基地数组
-     * @param array $date 时间段 ['2024-05-01','2024-05-31']
-     * @return array 学生uid，具体轮转科室，带教老师uid
+     * @param string $org_id 机构id 不能为空
+     * @param array $menu_id 轮转排班菜单数组 不能为空
+     * @param array $uid 学生数组，可以为空
+     * @param array $date 时间段 ['2024-05-01','2024-05-31'] 可以为空
+     * @param array $hd_id 本院科室数组 可以为空
+     * @param array $sd_id 标准科室数组 可以为空
+     * @param array $stage 轮转阶段数组 可以为空
+     * @param array $standard_base_id 基地数组 可以为空
+     * @return array 学生uid，时间段，具体轮转科室，带教老师uid
      */
-    public function getLeaveKsUsers(string $org_id,array $menu_id,array $hd_id,array $sd_id,array $stage, array $standard_base_id,array $date):array;
+    public function getLeaveKsUsers(string $org_id,array $menu_id,array $uid=[],array $date=[],array $hd_id=[],array $sd_id=[],array $stage=[], array $standard_base_id=[]):array;
 
 }
