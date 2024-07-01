469439ef4b16c109bc0aceadadade4fc7d6e8162
赵东升 <690617211@qq.com>
2024-07-01 13:45:44 +0800

    综合评估rpc修改

--- src/JsonRpc/ComprehensiveAssessmentServiceInterface.php
+++ src/JsonRpc/ComprehensiveAssessmentServiceInterface.php
@@ -23,7 +23,7 @@ interface ComprehensiveAssessmentServiceInterface
      *
      * @return array
      */
-    public function getMenuConfWhere(): array;
+    public function getMenuConfWhere(array $params): array;
 
     /**
      * 机构端设置项配置
