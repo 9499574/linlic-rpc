fea6c5e33a0f73d2ba39bad72dcec556ccfd202c
赵东升 <690617211@qq.com>
2024-07-08 13:59:39 +0800

    短信rpc

--- src/JsonRpc/SmsServiceInterface.php
+++ src/JsonRpc/SmsServiceInterface.php
@@ -11,4 +11,16 @@ interface SmsServiceInterface
      * @return bool
      */
     public function forgotPassword(string $phone, int $code):bool;
+
+    /**
+     * 发送短信
+     *
+     * @param string $phone 手机号
+     * @param array $params 短信模版参数
+     * @param string $templateCodeKey 模板key nacos配置中心message-center-sms.TemplateCodeList
+     * @return bool
+     * @user zhaodongsheng
+     * @time 2024/7/8 下午1:57
+     */
+    public function send(string $phone, array $params, string $templateCodeKey): bool;
 }
