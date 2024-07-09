d252464ba633d69df71ab1316a53111033febe87
thin <thin@thindeMacBook-Pro.local>
2024-07-09 13:51:21 +0800

    添加请假管理菜单配置项

--- src/JsonRpc/LeaveServiceInterface.php
+++ src/JsonRpc/LeaveServiceInterface.php
@@ -10,4 +10,27 @@ interface LeaveServiceInterface
      * @return array
      */
     public function getLeaveTypeOptions(array $params):array;
+
+    /*
+    * 请假管理-根据方法名和参数，调用rpc接口
+    * @param string $funcName
+    * @param array $params
+    * @return array
+    */
+    public function getFuncName(string $funcName,array $params):array;
+
+    /*
+     * 请假管理-请假列表-功能字段列表的接口
+     * @param array $params
+     * @return array
+     */
+    public function getFields(array $params):array;
+
+    /*
+     * 请假管理-请假列表-菜单设置的接口
+     * @param array $params
+     * @return array
+     */
+    public function getConfigs(array $params):array;
+
 }
