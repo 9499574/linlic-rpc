75ad7c094fe062898e80b8cd4297867442aad9cd
dbag <9499574@qq.com>
2024-08-01 15:18:53 +0800

    修改参数类型

--- src/JsonRpc/UserSystemMenuBtnServiceInterface.php
+++ src/JsonRpc/UserSystemMenuBtnServiceInterface.php
@@ -7,8 +7,8 @@ interface UserSystemMenuBtnServiceInterface
     /**
      * 获取用户系统菜单按钮列表
      * @param string $system_id
-     * @param string $menu_id
+     * @param array $menu_id
      * @return array
      */
-    public function getUserSystemMenuBtnList(string $system_id,string $menu_id):array;
+    public function getUserSystemMenuBtnList(string $system_id,array $menu_id):array;
 }
