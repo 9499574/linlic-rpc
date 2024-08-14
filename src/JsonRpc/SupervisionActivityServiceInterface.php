91c2cd679ec3cb6b22da3c07444a22459acc894b
zhaichunke <1347841607@qq.com>
2024-08-14 21:23:02 +0800

    隐藏督导活动配置

--- src/JsonRpc/SupervisionActivityServiceInterface.php
+++ src/JsonRpc/SupervisionActivityServiceInterface.php
@@ -10,10 +10,6 @@ interface SupervisionActivityServiceInterface
      * @return array
      */
     public function supervisionField(array $params):array;
-
-    public function taskConf(array $params):array;
-    public function supervisionTaskAutoField(array $params):array;
-    public function taskAutoConf(array $params):array;
     
 
 }
