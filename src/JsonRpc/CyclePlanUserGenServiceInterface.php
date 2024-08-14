c3e34b6dac554458272dc1630853ce4c41b7e614
dbag <9499574@qq.com>
2024-08-14 09:41:39 +0800

    用户已生成的出科数据

---
+++ src/JsonRpc/CyclePlanUserGenServiceInterface.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace linlic\JsonRpc;
+
+interface CyclePlanUserGenServiceInterface
+{
+    /**
+     * 获取生成的数据
+     * @param int $org_id
+     * @param array $hd_id
+     * @param array $uid
+     * @param string $start_date
+     * @param string $end_date
+     * @param array $field
+     * @param string $menu_id
+     * @return array
+     */
+    public function getUserGenInfo(int $org_id,array $hd_id,array $uid,string $start_date,string $end_date,array $field=['*'],string $menu_id=''):array;
+}
