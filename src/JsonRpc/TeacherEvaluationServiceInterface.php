72f22d5ca22a9ed886fb64ee8f9b6a7f0b532590
shenyilong <shenyilong@linlic.onaliyun.com>
2024-09-02 16:38:01 +0800

    增加师资评估配置接口

---
+++ src/JsonRpc/TeacherEvaluationServiceInterface.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace linlic\JsonRpc;
+
+interface TeacherEvaluationServiceInterface
+{
+
+    /**
+     * 师资评估-模块字段
+     * @param $params ['org_id'=>-1]
+     * @return array
+     */
+    public function teacherEvaluationField($params):array;
+
+    /**
+     * 师资评估-配置字段
+     * @param $params ['org_id'=>-1]
+     * @return array
+     */
+    public function teacherEvaluationConf($params):array;
+}
