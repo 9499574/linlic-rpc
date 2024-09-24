ea76ad74366a58ab23679a29dd0abeb389213a9c
364096849@qq.com <364096849@qq.com>
2024-09-24 17:05:46 +0800

    师资管理rpc接口

--- src/JsonRpc/TeacherQualificationServiceInterface.php
+++ src/JsonRpc/TeacherQualificationServiceInterface.php
@@ -6,14 +6,14 @@ interface TeacherQualificationServiceInterface
 {
 
     /**
-     * 师资资质-模块字段
+     * 师资管理-模块字段
      * @param $params ['org_id'=>-1]
      * @return array
      */
     public function teacherQualificationField($params):array;
 
     /**
-     * 师资资质-配置字段
+     * 师资管理-配置字段
      * @param $params ['org_id'=>-1]
      * @return array
      */
