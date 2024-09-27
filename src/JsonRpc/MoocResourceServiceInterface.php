a7498d45f56ac934986a4929fc7c05a70fce69e0
thin <thin@thindeMacBook-Pro.local>
2024-09-27 17:35:42 +0800

    moocResourceService添加

--- src/JsonRpc/MoocResourceServiceInterface.php
+++ src/JsonRpc/MoocResourceServiceInterface.php
@@ -4,26 +4,30 @@ namespace linlic\JsonRpc;
 
 interface MoocResourceServiceInterface
 {
-    /*
-    * 学习资源-根据方法名和参数，调用rpc接口
-    * @param string $funcName
-    * @param array $params
-    * @return array
-    */
+    /**
+     * @param string $funcName
+     * @param array $params
+     * @return array
+     */
     public function getFuncName(string $funcName,array $params):array;
 
-    /*
-     * 学习资源列表-功能字段列表的接口
+    /**
      * @param array $params
      * @return array
      */
     public function getFields(array $params):array;
 
-    /*
-     * 学习资源列表-菜单设置的接口
+    /**
      * @param array $params
      * @return array
      */
     public function getConfigs(array $params):array;
+
+    /**
+     * 获取学习资源详情
+     * @param array $resourceIds
+     * @return array
+     */
+    public function getResourceInfo(array $resourceIds): array;
 
 }
