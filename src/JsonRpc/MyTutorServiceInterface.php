2a71998a9b002dccd453c54db6f6240908c3841c
杨贵龙 <yangguilong@LAPTOP-N7CH0E44.localdomain>
2024-10-12 17:37:41 +0800

    我的导师

--- src/JsonRpc/MyTutorServiceInterface.php
+++ src/JsonRpc/MyTutorServiceInterface.php
@@ -9,12 +9,12 @@ interface MyTutorServiceInterface
      * @param $params ['org_id'=>-1]
      * @return array
      */
-    public function getFields(array $params):array;
+    public function myTutorServiceField(array $params):array;
 
     /**
      * 功能配置
      * @param array $params
      * @return array
      */
-    public function getConfigs(array $params):array;
+    public function myTutorServiceConf(array $params):array;
 }
