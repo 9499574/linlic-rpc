8d3a04a2b765c2c01f5dc6f785e3be9dc25afc75
zhichuangxin <924535840@qq.com>
2024-10-15 13:14:15 +0800

    耗材库存功能字段和功能配置

---
+++ src/JsonRpc/ConsumServiceInterface.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace linlic\JsonRpc;
+//耗材库存
+interface ConsumServiceInterface
+{
+    /**
+     * 功能字段
+     * @param $params
+     * @return array
+     */
+    public function getFields($params):array;
+
+    /**
+     * 功能配置
+     * @param $params
+     * @return array
+     */
+    public function getConfigs($params):array;
+
+}
