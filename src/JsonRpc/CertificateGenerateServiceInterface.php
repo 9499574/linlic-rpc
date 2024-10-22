ebb44f3405f7f398f0969ca95f43d195ae3412e2
liujiaxiang <9499574@qq.com>
2024-10-22 11:29:51 +0800

    生成的模板编号获取RPC配置信息

--- src/JsonRpc/CertificateGenerateServiceInterface.php
+++ src/JsonRpc/CertificateGenerateServiceInterface.php
@@ -7,4 +7,6 @@ interface CertificateGenerateServiceInterface
     public function getFields(array $params):array;
 
     public function getConf(array $params):array;
+
+    public function generateTemplateIdGetRpcParams(string $generate_template_id):array;
 }
