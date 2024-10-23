4b1a942eb6a26801dae740cd8ee7842f42b78106
364096849@qq.com <364096849@qq.com>
2024-10-23 11:31:49 +0800

    空间预约rpc接口

---
+++ src/JsonRpc/SpaceReservationServiceInterface.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace linlic\JsonRpc;
+
+interface SpaceReservationServiceInterface
+{
+
+    /**
+     * 空间预约-模块字段
+     * @param $params ['org_id'=>-1]
+     * @return array
+     */
+    public function spaceReservationField($params):array;
+
+    /**
+     * 空间预约-配置字段
+     * @param $params ['org_id'=>-1]
+     * @return array
+     */
+    public function spaceReservationConf($params):array;
+
+    /**
+     * 获取空间预约设置配置
+     * @param array $params
+     * @return array
+     */
+    public function getSetConf(array $params):array;
+}
