269d8057d6b2f050811f5525d90d1ca683e4e307
Evan <1208543289@qq.com>
2024-10-29 16:24:58 +0800

    设备详情

---
+++ src/JsonRpc/EquipmentUsingInfoServiceInterface.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace linlic\JsonRpc;
+
+interface EquipmentUsingServiceInterface
+{
+    /**
+     * 设备使用功能字段
+     * @param $params ['org_id'=>-1]
+     * @return array
+     */
+    public function usingField(array $params):array;
+
+    /**
+     * 设备使用功能配置
+     * @param array $params
+     * @return array
+     */
+    public function usingConf(array $params):array;
+}
