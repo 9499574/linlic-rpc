9d5094de4ab6cbf0ca64739170ea60b16552ae2b
thin <thin@thindeMacBook-Pro.local>
2024-10-30 17:44:44 +0800

    设备外借审核回调

--- src/JsonRpc/EquipmentLendServiceInterface.php
+++ src/JsonRpc/EquipmentLendServiceInterface.php
@@ -17,4 +17,32 @@ interface EquipmentLendServiceInterface
      * @return array
      */
     public function lendConf(array $params):array;
+
+    /**
+     * 外借流程审核回调
+     * @param array $params
+     * @return bool
+     */
+    public function lendAuditCallback(array $params): bool;
+
+    /**
+     * 归还流程审核回调
+     * @param array $params
+     * @return bool
+     */
+    public function returnAuditCallback(array $params): bool;
+
+    /**
+     * 外借流程参数
+     * @param array $params
+     * @return array
+     */
+    public function lendFlowParams(array $params): array;
+
+    /**
+     * 归还流程参数
+     * @param array $params
+     * @return array
+     */
+    public function returnFlowParams(array $params): array;
 }
