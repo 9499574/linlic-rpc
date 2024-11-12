7056cd21b05e0963fcbf5e255810514badd1350d
liujiaxiang <9499574@qq.com>
2024-11-12 19:36:01 +0800

    增加表单字典rpc接口

--- src/JsonRpc/FormDictionaryServiceInterface.php
+++ src/JsonRpc/FormDictionaryServiceInterface.php
@@ -10,8 +10,16 @@ interface FormDictionaryServiceInterface
     /**
      * 获取表单选项
      * 需要nacos里面配置，指定表单的id和key、value的key
-     * @param mixed ...$arguments
+     * @param string $orgId
+     * @param int $systemId
+     * @param string $menuId
+     * @param array $select
+     * @param array $where
+     * @param int $limit
+     * @param int $page
+     * @param string $keyword
+     * @param string $dict_key
      * @return array
      */
-    public function get(...$arguments):array;
+    public function getOptions(string $orgId, int $systemId, string $menuId, array $select = [], array $where = [], int $limit=0, int $page=0, string $keyword='', string $dict_key=''):array;
 }
