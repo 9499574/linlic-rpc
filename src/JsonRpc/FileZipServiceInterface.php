5a2a0519e8caa5560ee29932091ffc16412d19ec
liujiaxiang <9499574@qq.com>
2024-12-11 13:41:37 +0800

    通过资源ID压缩文件

--- src/JsonRpc/FileZipServiceInterface.php
+++ src/JsonRpc/FileZipServiceInterface.php
@@ -9,9 +9,9 @@ interface FileZipServiceInterface
 {
     /**
      * 通过资源ID压缩文件
-     * @param String $fileName 文件名（无需后缀）
+     * @param String $requestRecordId 生成的下载请求id
      * @param String $redisKey resourceIdFileZip.xxxx 对应数据格式为 [{resource_id:xxx, file_name:xxx.jpg}]
      * @return bool
      */
-    function resourceIdFileZip(String $fileName, String $redisKey):bool;
+    function resourceIdFileZip(string $requestRecordId, String $redisKey):bool;
 }
