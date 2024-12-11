e3da2885fb680e9a68c95a74f150f1e02d277b6e
zhichuangxin <924535840@qq.com>
2024-12-11 15:12:43 +0800

    结业申请字段配置

---
+++ src/JsonRpc/GradApplyServiceInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace linlic\JsonRpc;
+// 结业申请
+interface GradApplyServiceInterface
+{
+    /**
+     * 功能字段
+     * @param $params
+     * @return array
+     */
+    public function getFields($params):array;
+
+}
