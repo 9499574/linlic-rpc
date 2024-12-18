febed0f347df67223c4837d4474542c0e6c9e2a5
thin <thin@thindeMacBook-Pro.local>
2024-12-18 11:14:18 +0800

    设备使用确认rpc返回值修改

--- src/JsonRpc/EquipmentUsingServiceInterface.php
+++ src/JsonRpc/EquipmentUsingServiceInterface.php
@@ -26,13 +26,13 @@ interface EquipmentUsingServiceInterface
      * @param array $usingInfo ['start_time' => '2024-01-01 00:00:00', 'end_time' => '2024-02-01 00:00:00', 'location' => '使用地点', 'description' => '用途']
      * @param array $equipmentDetail [['equipment_id' => '717698815025336321', 'use_num' => 1, 'room_id' => '712979151396208640'], ['equipment_id' => '713064705249107968', 'use_num' => 2, 'room_id' => '714805298250108928']]
      * @param bool $randomRoom true：库存点数量不满足时，是否随机选择其余库存点，false：不可更换库存点
-     * @return array  code:1失败，0成功；msg:失败信息；data：recordIds 使用记录ID数组，usingInfo 使用明细
+     * @return array  code:1失败，0成功；msg:失败信息；data：recordIds 使用记录ID数组，equipmentDetail 使用设备明细
      * [
      *  'code' => 0,
      *  'msg' => '',
      *  'data' => [
      *      'recordIds'=>['711952656657465344', '711952656661659648', '711952656670048256'],
-     *      'usingInfo'=>[
+     *      'equipmentDetail'=>[
      *          ['equipment_room_id' => '717698815025336321_712979151396208640', 'equipment_id' => '717698815025336321', 'use_num' => 1, 'room_id' => '712979151396208640'],
      *          ['equipment_room_id' => '717698815025336321_712979151396208640', 'equipment_id' => '717698815025336321', 'use_num' => 1, 'room_id' => '712979151396208640']
      *      ]
