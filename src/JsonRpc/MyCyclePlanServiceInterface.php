0a22893a569fe6d3945836c55bba38ede3125326
liujiaxiang <9499574@qq.com>
2025-01-06 17:49:54 +0800

    获取字段选项

--- src/JsonRpc/MyCyclePlanServiceInterface.php
+++ src/JsonRpc/MyCyclePlanServiceInterface.php
@@ -5,4 +5,7 @@ namespace linlic\JsonRpc;
 interface MyCyclePlanServiceInterface
 {
     public function getFields(array $params):array;
+
+
+    public function getFieldOptions(array $params):array;
 }
