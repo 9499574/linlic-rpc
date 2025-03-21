5c6f388e1bb4a7882d6a4b5c911cbcd3e36024f6
tianlushang <17321377892@163.com>
2025-03-21 15:59:32 +0800

    招录管理-门户设置

--- src/JsonRpc/AdmissionAppleServiceInterface.php
+++ src/JsonRpc/AdmissionAppleServiceInterface.php
@@ -5,4 +5,11 @@ namespace linlic\JsonRpc;
 interface AdmissionAppleServiceInterface
 {
     public function formSubmitCallback(array $params):array;
+
+    /**
+     * 功能配置
+     * @param array $params
+     * @return array
+     */
+    public function admissionApplyConf(array $params):array;
 }
