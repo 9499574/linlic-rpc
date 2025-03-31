c7a9980c04ef88831430cd0b23b9827744e339dc
tianlushang <17321377892@163.com>
2025-03-31 11:27:10 +0800

    考试-免考

--- src/JsonRpc/ExamServiceInterface.php
+++ src/JsonRpc/ExamServiceInterface.php
@@ -110,4 +110,17 @@ interface ExamServiceInterface
      * 获取考试规则配置项
      * */
     public function getExamRuleConf(string $orgId, array $menuId): array;
+
+    /**
+     * 免考流程回调
+     * @param array $params
+     * @return array
+     */
+    public function exemptExamFlowCallBack(array $params): array;
+    /**
+     * 免考申请表单提交回调
+     * @param array $params
+     * @return array
+     */
+    public function exemptExamFormSubmitCallback(array $params): array;
 }
