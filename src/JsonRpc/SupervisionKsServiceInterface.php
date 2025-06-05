f32bc55b76cb99804e9502378354a36b0e3a1234
zck <zhaichunke@linlic.onaliyun.com>
2025-06-05 14:44:13 +0800

    新增科室督导相关RPC

---
+++ src/JsonRpc/SupervisionKsServiceInterface.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace linlic\JsonRpc;
+
+interface SupervisionKsServiceInterface
+{
+    /**
+     * 科室督导-功能字段
+     * @param array $params
+     * @return array
+     */
+    public function supervisionKsFields(array $params):array;
+
+    /**
+     * 获取设置配置
+     * @param array $params
+     * @return array
+     */
+    public function getSetConf(array $params):array;
+
+    /**
+     * 打分回调
+     * @param array $params
+     * @return array
+     */
+    public function formScoreCallback(array $params):array;
+
+    /**
+     * 督导组功能字段
+     * @param array $params
+     * @return array
+     */
+    public function supervisionTeamFields(array $params):array;
+
+    /**
+     * 督导组菜单设置
+     * @param array $params
+     * @return array
+     */
+    public function supervisionTeamConf(array $params):array;
+
+}
