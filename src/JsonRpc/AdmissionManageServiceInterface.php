b82e1b9639272512d2bdb76539dd45f34757ad5f
liujiaxiang <9499574@qq.com>
2025-06-10 13:31:33 +0800

    招录证书

--- src/JsonRpc/AdmissionManageServiceInterface.php
+++ src/JsonRpc/AdmissionManageServiceInterface.php
@@ -55,4 +55,41 @@ interface AdmissionManageServiceInterface
     public function getFilterOptions(array $params, string $key):array;
 
 
+    /**
+     * 获取证书模块参数
+     * @param array $params
+     * @return array
+     */
+    public function certificateModuleParams(array $params):array;
+
+    /**
+     * 获取证书模块数据
+     * @param array $params
+     * @return array
+     */
+    public function certificateModuleDetail(array $params):array;
+
+
+    /**
+     * 获取证书模块列表数据
+     * @param array $params
+     * @return array
+     */
+    public function certificateModuleListData(array $params):array;
+
+
+    /**
+     * 获取证书模块选择用户条件
+     * @param array $params
+     * @return array
+     */
+    public function certificateSelectUserWhere(array $params):array;
+
+    /**
+     * 获取证书模块选择用户列表
+     * @param array $params
+     * @return array
+     */
+    public function certificateSelectUserList(array $params):array;
+
 }
