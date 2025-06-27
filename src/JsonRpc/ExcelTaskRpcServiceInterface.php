fac562af1a5f2311cd9c9c1fa08d04e6fd4c18ae
liujiaxiang <9499574@qq.com>
2025-06-27 14:24:20 +0800

    excel智能解析

--- src/JsonRpc/ExcelTaskRpcServiceInterface.php
+++ src/JsonRpc/ExcelTaskRpcServiceInterface.php
@@ -5,4 +5,11 @@ namespace linlic\JsonRpc;
 interface ExcelTaskRpcServiceInterface
 {
     public function menuConf(array $params):array;
+
+    /**
+     * 获取主表数据
+     * @param array $params
+     * @return array
+     */
+    public function getExcelTaskList(array $params):array;
 }
