7f762f42295f8352ba58bc6e13ba1410af4bf047
zcx <924535840@qq.com>
2025-07-08 18:22:00 +0800

    报名管理功能字段、功能配置、审核流程rpc接口

---
+++ src/JsonRpc/EnrollServiceInterface.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace linlic\JsonRpc;
+
+interface EnrollServiceInterface
+{
+    /**
+     * 功能字段
+     * @param $params
+     * @return array
+     */
+    public function getFields($params):array;
+
+    /**
+     * 功能配置
+     * @param $params
+     * @return array
+     */
+    public function getConfigs($params):array;
+
+    /**
+     * 流程参数
+     * @param array $params
+     * @return array
+     */
+    public function flowParams(array $params): array;
+
+    /**
+     * 流程回调
+     * @param array $params
+     * @return bool
+     */
+    public function flowCallBack(array $params): bool;
+
+}
