3fbba3a902b4e19a60793709f220af304cd63fc3
zcx <924535840@qq.com>
2025-07-11 09:23:40 +0800

    证书接口，报名表单填写回调接口

--- src/JsonRpc/EnrollServiceInterface.php
+++ src/JsonRpc/EnrollServiceInterface.php
@@ -32,7 +32,54 @@ interface EnrollServiceInterface
      */
     public function flowCallBack(array $params): bool;
 
-    //获取常量选择
+    /**
+     * 获取常量选择
+     * @return array
+     */
     public function getConstOptions():array;
+
+    /**
+     * 获取证书模块参数
+     * @param array $params
+     * @return array
+     */
+    public function certificateModuleParams(array $params):array;
+
+    /**
+     * 获取证书模块数据
+     * @param array $params
+     * @return array
+     */
+    public function certificateModuleDetail(array $params):array;
+
+
+    /**
+     * 获取证书模块列表数据
+     * @param array $params
+     * @return array
+     */
+    public function certificateModuleListData(array $params):array;
+
+
+    /**
+     * 获取证书模块选择用户条件
+     * @param array $params
+     * @return array
+     */
+    public function certificateSelectUserWhere(array $params):array;
+
+    /**
+     * 获取证书模块选择用户列表
+     * @param array $params
+     * @return array
+     */
+    public function certificateSelectUserList(array $params):array;
+
+    /**
+     * 报名表单填写回调接口
+     * @param array $params
+     * @return array
+     */
+    public function formSubmitCallback(array $params):array;
 
 }
