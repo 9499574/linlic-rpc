9f429ae34692e88010206c87669e17f074d225ef
liujiaxiang <9499574@qq.com>
2025-08-01 16:21:02 +0800

    考勤排班

--- src/JsonRpc/SchedulingServiceInterface.php
+++ src/JsonRpc/SchedulingServiceInterface.php
@@ -5,21 +5,21 @@ namespace linlic\JsonRpc;
 interface SchedulingServiceInterface
 {
     /**
-     * 考勤排班功能字段
+     * 考勤排班功能字段1
      * @param array $params
      * @return array
      */
     public function getFields(array $params) :array;
 
     /**
-     * 考勤排班菜单配置
+     * 考勤排班菜单配置2
      * @param array $params
      * @return array
      */
     public function menuConf(array $params):array;
 
     /**
-     * 考勤排班机构设置
+     * 考勤排班机构设置3
      * @param array $params
      * @return array
      */
