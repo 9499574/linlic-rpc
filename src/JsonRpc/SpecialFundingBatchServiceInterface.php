bd88bcca577a6e23404c5d68ba95a1df77f5e9de
陈奕文 <1447292956@qq.com>
2025-08-12 10:46:30 +0800

    专项经费批量导入rpc

---
+++ src/JsonRpc/SpecialFundingBatchServiceInterface.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace linlic\JsonRpc;
+
+interface SpecialFundingBatchServiceInterface
+{
+    /**
+     * 模板 RPC
+     * org_id string 机构编号
+     * system_id string 系统编号
+     * menu_id string 菜单编号
+     * pre_params array 前置页保存参数
+     * @param array $params
+     * @return array
+     */
+    public function templateConfig(array $params): array;
+
+    /**
+     * 校验 RPC
+     *  org_id string 机构编号
+     *  system_id string 系统编号
+     *  menu_id string 菜单编号
+     *  pre_params array 前置页保存参数
+     * @param array $params
+     * @return array
+     */
+    public function validConfig(array $params) :array;
+
+    /**
+     * 导入 RPC
+     * 接口请求参数：redisKey
+     * cache里面存放如下数据：
+     * params:org_id、system_id、menu_id、pre_params、uid
+     * data:["sheet1"=>[["user_name"=>"1111"]]]
+     * @param array $params
+     * @return array
+     */
+    public function importData(array $params): array;
+}
