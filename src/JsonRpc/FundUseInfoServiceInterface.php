4be6fc18fbe7bde35efff6b9a552d3e15acf7440
陈奕文 <1447292956@qq.com>
2025-08-15 13:13:21 +0800

    资金使用信息rpc

---
+++ src/JsonRpc/FundUseInfoServiceInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace linlic\JsonRpc;
+
+interface FundUseInfoServiceInterface
+{
+    /**
+     * 资金使用信息功能字段
+     * @param array $params
+     * @return array
+     */
+    public function getFields(array $params) :array;
+}
