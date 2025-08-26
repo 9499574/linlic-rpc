9a928e196b34069209f6870b478682485abc15e4
TBLD <8888491+tblds@user.noreply.gitee.com>
2025-08-26 09:06:16 +0800

    技能考核RPC

---
+++ src/JsonRpc/ClinicalSkillsCheckServiceInterface.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace linlic\JsonRpc;
+
+interface ClinicalSkillsCheckServiceInterface
+{
+    /**
+     * 技能考核功能字段
+     * @param array $params
+     * @return array
+     */
+    public function getFields(array $params) :array;
+
+    /**
+     * 技能考核菜单配置
+     * @param array $params
+     * @return array
+     */
+    public function menuConf(array $params):array;
+
+    /**
+     * 技能考核机构设置
+     * @param array $params
+     * @return array
+     */
+    public function SetConf(array $params):array;
+}
