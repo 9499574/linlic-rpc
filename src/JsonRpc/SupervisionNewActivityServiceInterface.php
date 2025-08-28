aa345b67622987cb785c547374b8da7a2a604530
lxt <15681165132@163.com>
2025-08-28 10:59:00 +0800

    新增活动督导接口配置

---
+++ src/JsonRpc/SupervisionNewActivityServiceInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace linlic\JsonRpc;
+
+interface SupervisionNewActivityServiceInterface
+{
+    /**
+     * 功能字段
+     * @param array $params
+     * @return array
+     */
+    public function supervisionField(array $params):array;
+    
+
+}
