088673572dc2cd286b85362326aeb84a036eff4e
陈艳权 <11214860+chenyanquan110@user.noreply.gitee.com>
2025-08-28 11:33:43 +0800

    试题练习 获取是否开启云管家题库、本院题库分类

--- src/JsonRpc/PracticeQuestionServiceInterface.php
+++ src/JsonRpc/PracticeQuestionServiceInterface.php
@@ -2,14 +2,14 @@
 
 namespace linlic\JsonRpc;
 
-interface PracticeServiceInterface
+interface PracticeQuestionServiceInterface
 {
     /**
-     * 根据机构id，系统id、角色ids 获取是否开通了试题练习的 本院题库，云管家题库权限(优先角色id权限，其次是总体库开发比例)
+     * 根据机构id，菜单menu_id、角色ids 获取是否开通了试题练习的 本院题库，云管家题库权限(优先角色id权限，其次是总体库开发比例)
      * @param string $orgId 机构Id
      * @param string $systemId 系统Id
      * @param array $roleId 角色Id
      * @return array 返回的数据格式返回格式如：[["option_label"=>"云管家题库","option_value"=>"yunCate"],["option_label"=>"本院题库","option_value"=>"orgCate"]]
      */
-    public function getTopCateOptions(string $orgId, string $systemId, array $roleId = []): array;
+    public function getTopCateOptions(string $orgId, string $menuId, array $roleId = []): array;
 }
