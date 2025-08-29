0311176e13f923fa38985f77464b620e968d32d0
TBLD <8888491+tblds@user.noreply.gitee.com>
2025-08-29 15:52:44 +0800

    获取学习资源配置的题目

--- src/JsonRpc/MoocResourceServiceInterface.php
+++ src/JsonRpc/MoocResourceServiceInterface.php
@@ -30,4 +30,17 @@ interface MoocResourceServiceInterface
      */
     public function getResourceInfo(array $resourceIds): array;
 
+    /**
+     * 获取学习资源配置的题目
+     * @param array $resourceIds
+     * @param array $field
+     * field 字段释义
+     * question_ids 测试题目
+     * question_video_details 视频题目
+     * question_before_ids 考前测试题
+     * question_after_ids 考后测试题
+     * @return array
+     */
+    public function getResourceTopic(array $resourceIds,array $field = []): array;
+
 }
