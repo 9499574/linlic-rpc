dff1e839099204bbdbfc49980f217668897b1a58
陈奕文 <1447292956@qq.com>
2025-08-29 10:31:50 +0800

    师生互选详情rpc

---
+++ src/JsonRpc/TutorSelectDetailServiceInterface.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace linlic\JsonRpc;
+
+
+/**
+ * 师生互选详情
+ */
+interface TutorSelectDetailServiceInterface
+{
+    /**
+     * 功能字段
+     * @param array $params
+     * @return array
+     */
+    public function getFields(array $params) :array;
+
+    /**
+     * 菜单配置
+     * @param array $params
+     * @return array
+     */
+    public function menuConf(array $params):array;
+}
