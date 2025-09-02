c3dd8b76d5691b048fdf9f88d8a8f7228cd153c8
陈艳权 <11214860+chenyanquan110@user.noreply.gitee.com>
2025-09-02 10:08:43 +0800

    试题练习 获取云管家题库、本院题库分类 rpc

--- src/JsonRpc/PracticeQuestionServiceInterface.php
+++ src/JsonRpc/PracticeQuestionServiceInterface.php
@@ -11,4 +11,19 @@ interface PracticeQuestionServiceInterface
      * @return array 返回的数据格式返回格式如：[["option_label"=>"云管家题库","option_value"=>"yunCate"],["option_label"=>"本院题库","option_value"=>"orgCate"]]
      */
     public function getTopCateOptions(string $org_id, string $menu_id): array;
+    /**
+     * 根据菜单menu_id 获取是否开通了试题练习的 云管家题库权限(优先角色id权限，其次是总体题库开放比例(所有分类))
+     * @param string $menu_id 菜单Id
+     * @param string $pid 上级分类Id
+     * @return array 返回题库分类数组
+     */
+    public function getYunCateList(string $menu_id,string $pid=''): array;
+    /**
+     * 根据菜单menu_id 获取是否开通了试题练习的 本院题库，云管家题库权限(优先角色id权限，其次是总体题库开放比例其次是总体题库开放比例(所有分类))
+     * @param string $menu_id 菜单Id
+     * @param string $pid 上级分类Id
+     * @return array 返回题库分类数组
+     */
+    public function getOrgCateList(string $menu_id,string $pid=''): array;
+
 }
