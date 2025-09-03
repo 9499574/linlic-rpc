86ef346a4dfe726ace3fb858620a79009ebe10b2
陈艳权 <11214860+chenyanquan110@user.noreply.gitee.com>
2025-09-03 17:52:29 +0800

    试题练习 获取云管家题库、本院题库分类 rpc

--- src/JsonRpc/PracticeQuestionServiceInterface.php
+++ src/JsonRpc/PracticeQuestionServiceInterface.php
@@ -25,5 +25,12 @@ interface PracticeQuestionServiceInterface
      * @return array 返回题库分类数组
      */
     public function getOrgCateList(string $menu_id,string $pid=''): array;
-
+    /**
+     * 根据菜单menu_id ,题库分类ids 获取题型以及过滤后的实际题库分类ids(若是角色设置的子分类，则返回的是子分类集合)
+     * @param string $menu_id 菜单Id
+     * @param array $sc_ids 题库分类ids
+     * @param string $type 类型：1云管家题库，2本院题库
+     * @return array 返回过滤后题库分类数组、题型数组、题库开放比例百分比
+     */
+    public function getQuestionTypebyCate(string $menu_id,array $sc_ids,string $type): array;
 }
