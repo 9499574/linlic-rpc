56721893ec08899d24e74ca8a84f175fe2d5d555
陈奕文 <1447292956@qq.com>
2025-09-08 14:39:20 +0800

    师生关系菜单配置rpc

---
+++ src/JsonRpc/TeacherStudentRelation.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace linlic\JsonRpc;
+
+interface TeacherStudentRelation
+{
+    /**
+     * 菜单配置
+     * @param array $params
+     * @return array
+     */
+    public function menuConf(array $params):array;
+}
