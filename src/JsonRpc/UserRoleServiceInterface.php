a2e4cc830219334de83c5ee6ea1e4679bb1518be
佘鑫鑫 <tengpro@qq.com>
2025-09-28 10:05:11 +0800

    修改接口方法

--- src/JsonRpc/UserRoleServiceInterface.php
+++ src/JsonRpc/UserRoleServiceInterface.php
@@ -5,5 +5,5 @@ namespace linlic\JsonRpc;
 interface UserRoleServiceInterface
 {
     // 根据用户ID获取用户组
-    public function getRoleListByUid(array $params): array
+    public function getRoleListByUid(array $params): array;
 }
