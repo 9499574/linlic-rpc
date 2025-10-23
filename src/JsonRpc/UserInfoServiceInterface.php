1d60ee0e45c4ed606a5b3ad8087103bed20591ab
liujiaxiang <9499574@qq.com>
2025-10-23 09:46:52 +0800

    更新用户信息字段

--- src/JsonRpc/UserInfoServiceInterface.php
+++ src/JsonRpc/UserInfoServiceInterface.php
@@ -7,8 +7,8 @@ interface UserInfoServiceInterface
     /**
      * 更新用户信息字段(与之前的数据进行合并)
      * @param int $uid
-     * @param array $params ['a'=>1,'b'=>[1,2,3]]
+     * @param array $data ['a'=>1,'b'=>[1,2,3]]
      * @return bool
      */
-    public function updateUserInfoField(int $uid,array $params):bool;
+    public function updateUserInfoField(int $uid,array $data):bool;
 }
