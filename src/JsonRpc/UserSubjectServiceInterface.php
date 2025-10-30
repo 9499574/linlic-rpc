65d8e69d500166173faf9ba78a41f6208592efdf
杨焕军 <986273747@.com>
2025-10-30 17:27:44 +0800

    学科管理

---
+++ src/JsonRpc/UserSubjectServiceInterface.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace linlic\JsonRpc;
+
+interface UserSubjectServiceInterface
+{
+    /**
+     * 学科管理-模块字段
+     * @param $params ['org_id'=>-1]
+     * @return array
+     */
+    public function userSubjectField($params):array;
+
+    /**
+     * 学科管理-配置字段
+     * @param $params ['org_id'=>-1]
+     * @return array
+     */
+    public function userSubjectConf($params):array;
+}
