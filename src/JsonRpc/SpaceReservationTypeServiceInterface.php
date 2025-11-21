57e57f6317a0096e13d576f9bd8806e65515daab
陈艳权 <11214860+chenyanquan110@user.noreply.gitee.com>
2025-11-21 16:21:51 +0800

    空间预约类型rpc

--- src/JsonRpc/SpaceReservationTypeServiceInterface.php
+++ src/JsonRpc/SpaceReservationTypeServiceInterface.php
@@ -7,6 +7,7 @@ interface SpaceReservationTypeServiceInterface
 
     /**
      * 空间预约类型-模块字段
+     * @param array $params
      * @return array
      */
     public function getFields($params):array;
