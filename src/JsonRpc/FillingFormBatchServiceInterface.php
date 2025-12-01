07991a79fd8ac56501c3fca724b7bd50c5865b45
liujiaxiang <9499574@qq.com>
2025-12-01 10:21:10 +0800

    填报表单批量服务接口

--- src/JsonRpc/FillingFormBatchServiceInterface.php
+++ src/JsonRpc/FillingFormBatchServiceInterface.php
@@ -10,32 +10,40 @@ interface FillingFormBatchServiceInterface
 
     /**
      * 批量插入数据
+     * @param string $orgId
+     * @param string $uid
      * @param array $data
      * @return bool
      */
-    public function batchInsert(array $data):bool;
+    public function batchInsert(string $orgId,string $uid,array $data):bool;
 
     /**
      * 批量删除并插入数据
+     * @param string $orgId
+     * @param string $uid
      * @param array $data
      * @param array $deleteWhere
      * @return bool
      */
-    public function batchDeleteAndInsert(array $data,array $deleteWhere):bool;
+    public function batchDeleteAndInsert(string $orgId,string $uid,array $data,array $deleteWhere):bool;
 
     /**
      * 批量插入数据(通过redis传输数据)
+     * @param string $orgId
+     * @param string $uid
      * @param string $redisKey
      * @return bool
      */
-    public function redisBatchInsert(string $redisKey):bool;
+    public function redisBatchInsert(string $orgId,string $uid,string $redisKey):bool;
 
     /**
      * 批量删除并插入数据(通过redis传输数据)
+     * @param string $orgId
+     * @param string $uid
      * @param string $redisKey
      * @return bool
      */
-    public function redisBatchDeleteAndInsert(string $redisKey):bool;
+    public function redisBatchDeleteAndInsert(string $orgId,string $uid,string $redisKey):bool;
 
 
 
