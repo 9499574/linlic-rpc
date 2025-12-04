ac4ea1bea8ae750ab92d2f2cf2c1cfd4ae0e6337
liujiaxiang <9499574@qq.com>
2025-12-04 16:45:55 +0800

    模块短信公共接口

--- src/JsonRpc/ModuleSmsSendServiceInterface.php
+++ src/JsonRpc/ModuleSmsSendServiceInterface.php
@@ -7,5 +7,13 @@ namespace linlic\JsonRpc;
  */
 interface ModuleSmsSendServiceInterface
 {
+    /**
+     * 批量发送短信
+     * @param string $orgId
+     * @param string $smsKey
+     * @param array $userIds
+     * @param array $variable
+     * @return array ['status'=>0|1|2,'msg'=>''] 0是全部成功，1是部分成功，2是全部失败
+     */
     public function batchSendNoticeSms(string $orgId,string $smsKey,array $userIds,array $variable):array;
 }
