0f972333617e63fe5a03fc62dd5be9a1e49d042a
宇宙骑士 <1010707582@qq.com>
2025-12-08 09:48:54 +0800

    新增 获取指定月份未排班的人数统计 和 获取某一天没有排班的用户ID列表 接口

--- src/JsonRpc/SchedulingServiceInterface.php
+++ src/JsonRpc/SchedulingServiceInterface.php
@@ -32,4 +32,26 @@ interface SchedulingServiceInterface
      * @return array
      */
     public function getShiftSchedulingData(array $menuId, string $date):array;
+
+    /**
+     * 获取指定月份未排班的人数统计
+     * @param string $month Y-m
+     * @param string $orgId
+     * @param string $systemId
+     * @param string $menuId
+     * @param array $hdId
+     * @return array
+     */
+    public function countNoSchedulingUserNum(string $month, string $orgId, string $systemId, string $menuId, array $hdId = []): array;
+
+    /**
+     * 获取某一天没有排班的用户ID列表
+     * @param string $day Y-m-d
+     * @param string $orgId
+     * @param string $systemId
+     * @param string $menuId
+     * @param array $hdId
+     * @return array
+     */
+    public function getNoSchedulingUserIdsByDay(string $day, string $orgId, string $systemId, string $menuId, array $hdId = []): array;
 }
