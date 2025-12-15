9ac4a809bdce4c21139b3569a694ee6adeb19bde
liujiaxiang <9499574@qq.com>
2025-12-15 18:07:48 +0800

    模块数据统计-教学活动

--- src/JsonRpc/MSTeachingActivitiesServiceInterface.php
+++ src/JsonRpc/MSTeachingActivitiesServiceInterface.php
@@ -8,16 +8,30 @@ namespace linlic\JsonRpc;
 interface MSTeachingActivitiesServiceInterface
 {
     /**
-     * 维度类型
+     * 维度类型-个人
      * @param array $params
      * @return array
      */
-    public function dimension(array $params):array;
+    public function userDimension(array $params):array;
 
     /**
-     * 筛选条件
+     * 筛选条件-个人
      * @param array $params
      * @return array
      */
-    public function filter(array $params):array;
+    public function userFilter(array $params):array;
+
+    /**
+     * 维度类型-字典
+     * @param array $params
+     * @return array
+     */
+    public function dictDimension(array $params):array;
+
+    /**
+     * 筛选条件-字典
+     * @param array $params
+     * @return array
+     */
+    public function dictFilter(array $params):array;
 }
