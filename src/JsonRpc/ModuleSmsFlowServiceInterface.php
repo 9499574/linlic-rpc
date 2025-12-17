0eaf5a231ca42dc07f9c7d9e9f518a611421da15
liujiaxiang <9499574@qq.com>
2025-12-17 11:44:28 +0800

    模块短信发送-流程审核人

---
+++ src/JsonRpc/ModuleSmsFlowServiceInterface.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace linlic\JsonRpc;
+
+interface ModuleSmsFlowServiceInterface
+{
+    /**
+     * 获取模块短信服务配置参数
+     * @param int $orgId
+     * @param string $menuId
+     * @return array ['user_option'=>[['label'=>'','value'=>'']],'rule_option'=>[['label'=>'','value'=>'','children'=>[]]],'time_option'=>[['label'=>'','value'=>'']]]
+     */
+    public function getModuleConfigParams(int $orgId,string $menuId):array;
+
+
+    /**
+     * 获取模块短信服务过滤数据
+     * @param array $params
+     * @return array
+     */
+    public function getModuleFilterData(array $params):array;
+}
