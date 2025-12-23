0ffacac37abba5f67ac5f17d0621c69dee16938f
zck <zhaichunke@linlic.onaliyun.com>
2025-12-23 13:46:57 +0800

    教学活动提供证书相关rpc接口入参修改

--- src/JsonRpc/CertificateTeachingActivityServiceInterface.php
+++ src/JsonRpc/CertificateTeachingActivityServiceInterface.php
@@ -9,49 +9,38 @@ interface CertificateTeachingActivityServiceInterface
 {
     /**
      * 功能参数
-     * @param  $org_id
-     * @param  $menu_id
+     * @param $params
      * @return array
      */
-    public function getDomainParams($org_id,$menu_id):array;
+    public function getDomainParams($params):array;
 
     /**
      * 模块数据详情
-     * @param $org_id
-     * @param $menu_id
-     * @param $business_id
-     * @param array $user_ids
+     * @param $params
      * @return array
      */
-    public function getModuleDataDetail($org_id, $menu_id, $business_id, array $user_ids=[]):array;
+    public function getModuleDataDetail($params):array;
 
     /**
      * 获取模块列表数据
-     * @param $org_id
-     * @param $menu_id
-     * @param $page
-     * @param $limit
-     * @param array $search_params
+     * @param $params
      * @return array
      */
-    public function getModuleDataList($org_id, $menu_id, $page=1, $limit=10, array $search_params=[]):array;
+    public function getModuleDataList($params):array;
 
     /**
      * 模块功能规则
-     * @param $org_id
-     * @param $menu_id
-     * @return array[]
+     * @param $params
+     * @return array
      */
-    public function getModuleRule($org_id,$menu_id):array;
+    public function getModuleRule($params):array;
 
     /**
      * 模块人员列表
-     * @param $org_id
-     * @param $menu_id
-     * @param $business_id
-     * @return array[]
+     * @param $params
+     * @return array
      */
-    public function getModuleUser($org_id,$menu_id,$business_id):array;
+    public function getModuleUser($params):array;
 
 
 }
