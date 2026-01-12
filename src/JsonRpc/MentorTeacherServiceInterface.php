89f3e465511c7f8b2a4f1b9378fe7a7bf4bc8587
xuewei <james.xue@ccmtv.cn>
2026-01-12 10:50:51 +0800

    带教老师-我的学生功能字段RPC

---
+++ src/JsonRpc/MentorTeacherServiceInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace linlic\JsonRpc;
+
+interface MentorTeacherServiceInterface {
+
+    /**
+     * 功能字段
+     * @param array $params
+     * @return array
+     */
+    public function getFields($params): array;
+
+}
