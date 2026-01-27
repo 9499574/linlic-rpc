4c54400cf8b9c24c911bbad31863270d11bba6cd
liujiaxiang <9499574@qq.com>
2026-01-27 17:33:24 +0800

    获取流程节点人员列表

--- src/JsonRpc/FlowServiceInterface.php
+++ src/JsonRpc/FlowServiceInterface.php
@@ -4,5 +4,10 @@ namespace linlic\JsonRpc;
 
 interface FlowServiceInterface extends BaseModuleServiceInterface
 {
-
+    /**
+     * 获取流程节点人员列表
+     * @param array $flowIds
+     * @return array
+     */
+    public function getFlowNodeUserList(array $flowIds):array;
 }
