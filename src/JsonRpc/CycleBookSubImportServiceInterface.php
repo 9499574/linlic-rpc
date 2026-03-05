d06690ae336f185e9c13b0e78d8ac787a4294c60
xuewei <james.xue@ccmtv.cn>
2026-03-05 14:08:17 +0800

    轮转手册导入RPC

---
+++ src/JsonRpc/CycleBookSubImportServiceInterface.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace linlic\JsonRpc;
+
+interface CycleBookSubImportServiceInterface
+{
+
+    /**
+     * 校验RPC
+     * @param array $params
+     * @return array
+     */
+    public function validConfig(array $params): array;
+
+    /**
+     * 模板RPC
+     * @param array $params
+     * @return array
+     */
+    public function templateConfig(array $params): array;
+
+    /**
+     * 导入PRC
+     * @param array $params
+     * @return array
+     */
+    public function importData(array $params): array;
+
+}
