6fb987b08ad5dac649d9a19174f3bc631a21168d
zcx <924535840@qq.com>
2026-03-06 14:35:06 +0800

    房间预约类型数据初始化

--- src/JsonRpc/SpaceReservationTypeServiceInterface.php
+++ src/JsonRpc/SpaceReservationTypeServiceInterface.php
@@ -11,4 +11,11 @@ interface SpaceReservationTypeServiceInterface
      * @return array
      */
     public function getFields($params):array;
+
+    /**
+     * 房间预约类型数据初始化
+     * @param array $params
+     * @return bool
+     */
+    public function dataInit(array $params) :bool;
 }
