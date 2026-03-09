bea8a6d25292df64469b4de7e80fa031411e9cdb
liujiaxiang <9499574@qq.com>
2026-03-09 14:44:57 +0800

    机构端请假类型数据初始化

--- src/JsonRpc/LeaveTypeServiceInterface.php
+++ src/JsonRpc/LeaveTypeServiceInterface.php
@@ -9,6 +9,6 @@ interface LeaveTypeServiceInterface
     public function getConfigs(array $params):array;
 
 
-    public function dataInit(array $params):array;
+    public function dataInit(array $params):bool;
 
 }
