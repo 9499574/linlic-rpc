139fda075af317e7428e1e6544fdde68747cf733
陈奕文 <1447292956@qq.com>
2026-03-25 13:55:58 +0800

    年度考核导入新增前置rpc方法

--- src/JsonRpc/AnnualAssessmentBatchServicerInterface.php
+++ src/JsonRpc/AnnualAssessmentBatchServicerInterface.php
@@ -5,6 +5,15 @@ namespace linlic\JsonRpc;
 interface AnnualAssessmentBatchServicerInterface
 {
     /**
+     * 前置PRC
+     * org_id string 机构编号
+     * system_id string 系统编号
+     * menu_id string 菜单编号
+     * pre_params array 前置页保存参数
+     */
+    public function preFormConfig(array $params): array;
+
+    /**
      * 校验RPC
      * @param array $params
      * @return array
