1e878a91b8ee6539643f68582f3d5e67bf6064ad
liujiaxiang <9499574@qq.com>
2026-05-06 11:12:28 +0800

    通过标准或者本院获取关联增加系统ID限制条件

--- src/JsonRpc/SdHdRelationServiceInterface.php
+++ src/JsonRpc/SdHdRelationServiceInterface.php
@@ -8,17 +8,19 @@ interface SdHdRelationServiceInterface
      * 通过标准科室ID获取对应本院科室ID
      * @param array $sdIds
      * @param int $org_id
+     * @param int|null $system_id
      * @return array ['111'=>[111,2222],'2222'=>[3333,444]]
      */
-    public function getSdIdByHdId(array $sdIds,int $org_id):array;
+    public function getSdIdByHdId(array $sdIds,int $org_id, int $system_id = null):array;
 
     /**
      * 通过本院科室ID获取对应标准科室ID
      * @param array $hdIds
      * @param int $org_id
+     * @param int|null $system_id
      * @return array ['111'=>[111,2222],'2222'=>[3333,444]]
      */
-    public function getHdIdBySdId(array $hdIds,int $org_id):array;
+    public function getHdIdBySdId(array $hdIds,int $org_id, int $system_id = null):array;
 
     /**
      * 本院科室与标准科室关联选项 返回级联数据格式
