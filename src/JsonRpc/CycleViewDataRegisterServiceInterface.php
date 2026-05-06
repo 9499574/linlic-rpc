aa3dd474f36e1be2c14a5bf8259717915dc50e58
陈艳权 <11214860+chenyanquan110@user.noreply.gitee.com>
2026-05-06 18:35:13 +0800

    汇总表注册 轮转总览-带教学员 字段RPC、列表RPC

--- src/JsonRpc/CycleViewDataRegisterServiceInterface.php
+++ src/JsonRpc/CycleViewDataRegisterServiceInterface.php
@@ -20,4 +20,19 @@ interface CycleViewDataRegisterServiceInterface
      * @return array
      */
     public function registerWithStudent(string $orgId, array $menuId):array;
+    /**
+     * 带教学员-字段
+     * @param string $orgId
+     * @param array $menuId
+     * @return array
+     */
+    public function registerWithStudentField(string $orgId, array $menuId):array;
+    /**
+     * 带教学员-列表
+     * @param string $orgId
+     * @param array $businessId
+     * @param array $fields
+     * @return array
+     */
+    public function registerWithStudentDetail(string $orgId,array $businessId,array $fields):array;
 }
