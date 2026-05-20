7cef67cdcf5320ab4b2ec71c6e0d748348abb651
xuewei <james.xue@ccmtv.cn>
2026-05-20 16:13:11 +0800

    本院科室+考试名称 选项

--- src/JsonRpc/TeachingActivityServiceInterface.php
+++ src/JsonRpc/TeachingActivityServiceInterface.php
@@ -385,4 +385,11 @@ interface TeachingActivityServiceInterface
      * */
     public function getUserActivityCount(int $org_id,string $system_id,string $uid,string $start_date = '',string $end_date = ''): int;
 
+    /**
+     * 个人参与教学活动相关配置字段内容-数据汇总
+     * @param $params
+     * @return array
+     */
+    public function getUserJoinActivityRuleConf(string $orgId, array $menuId): array;
+
 }
