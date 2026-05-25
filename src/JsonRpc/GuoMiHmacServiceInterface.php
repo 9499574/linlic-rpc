ce4b2e21b0880eb67f30ec9dc03b24a79f920e4a
liujiaxiang <9499574@qq.com>
2026-05-25 15:55:02 +0800

    国密HMAC接口

--- src/JsonRpc/GuoMiHmacServiceInterface.php
+++ src/JsonRpc/GuoMiHmacServiceInterface.php
@@ -4,7 +4,7 @@ namespace linlic\JsonRpc;
 
 interface GuoMiHmacServiceInterface
 {
-    public function saveDataIntegrityRecordHmac(string $orgId, string $bizType, int $bizId, array $plainTextData):bool;
+    public function saveDataIntegrityRecordHmac(string $orgId, string $bizType, string $bizId, array $plainTextData):bool;
 
-    public function checkDataIntegrityRecordHmac(string $orgId, string $bizType, int $bizId, array $plainTextData):bool;
+    public function checkDataIntegrityRecordHmac(string $orgId, string $bizType, string $bizId, array $plainTextData):bool;
 }
