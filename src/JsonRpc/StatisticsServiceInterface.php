75bcf9014ce820cc178cfec68d173bbb63e2a9bb
xuewei <james.xue@ccmtv.cn>
2026-06-02 14:45:13 +0800

    科室进度看板RPC

--- src/JsonRpc/StatisticsServiceInterface.php
+++ src/JsonRpc/StatisticsServiceInterface.php
@@ -13,4 +13,14 @@ interface StatisticsServiceInterface
      * 个人进度看板-配置字段
      */
     public function personalProgressDashboardConf($params): array;
+
+    /**
+     * 科室进度看板-模块字段
+     */
+    public function ksProgressDashboardField($params): array;
+
+    /**
+     * 科室进度看板-配置字段
+     */
+    public function ksProgressDashboardConf($params): array;
 }
