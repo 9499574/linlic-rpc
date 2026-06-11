fde3d437f5986c8210bd47609264c593c7d77761
zcx <924535840@qq.com>
2026-06-11 17:58:39 +0800

    根据查询条件获取出科数据

--- src/JsonRpc/CycleCkServiceInterface.php
+++ src/JsonRpc/CycleCkServiceInterface.php
@@ -19,4 +19,11 @@ interface CycleCkServiceInterface
      */
     public function viewConf(array $params):array;
 
+    /**
+     * 根据查询条件获取出科数据
+     * @param array $params ['system_id'=>'系统ID','menu_id'=>'菜单ID','type'=>'科室类型 1-本院科室，2-标准科室','ck_type'=>'出科类型 1-当前出科，2-历史出科','search_params'=>[筛选条件]]
+     * @return array
+     */
+    public function getCkData(array $params):array;
+
 }
